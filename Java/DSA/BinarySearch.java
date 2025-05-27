39bee48131aa558f860e1fda0486f276191f29c4
Noah <106123749+nbran3@users.noreply.github.com>
2025-05-26 20:47:41 -0400

    Create BinarySearch.java

---
+++ Java/DSA/BinarySearch.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+// Binary Search = search algorithm that find the position of a target value within a sorted array. 
+// Half of the array is disregarded during each "step"
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int array[] = new int[1000000];
+    int target = 74391;
+
+    for (int i = 0; i<array.length; i++){
+        array[i] = i;
+    }
+
+    //int index = Arrays.binarySearch(array, target);
+        int index = binarySearch(array, target);
+
+
+        if(index == -1){
+        System.out.println(target +" not found");
+    }else{
+        System.out.println("Element found at: " + index);
+    }
+
+    }
+
+    private static int binarySearch(int[] array, int target) {
+
+        int low = 0;
+        int high = array.length - 1;
+        int steps = 0;
+
+        while (low <= high){
+            steps ++;
+            int middle = low + (high -low ) /2;
+            int value = array[middle];
+            System.out.println("middle: " + value + " steps: " + steps);
+
+            if(value < target){
+                low = middle + 1;
+            } else if(value> target) high = middle -1;
+            else return middle;
+
+
+        }            return -1;
+
+
+    }
+
+}
