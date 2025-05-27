c3d75dcf48d7b567ce8bed259081b2f180f164f7
Noah <106123749+nbran3@users.noreply.github.com>
2025-05-26 21:18:43 -0400

    Create BubbleSort.java

---
+++ Java/DSA/BubbleSort.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int array[] = {9, 2, 7, 6, 1, 5, 4, 3, 8};
+
+
+        bubbleSort(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
+
+    }
+
+    private static void bubbleSort(int array[]) {
+
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+
+                }
+
+            }
+        }
+
+    }
+}
