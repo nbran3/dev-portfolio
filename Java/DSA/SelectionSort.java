045fc2637cc8f5d66cbdbc4fa25836e30e840235
Noah <106123749+nbran3@users.noreply.github.com>
2025-05-27 13:04:35 -0400

    Create SelectionSort.java

---
+++ Java/DSA/SelectionSort.java
@@ -0,0 +1,32 @@
+// selection sort = search through an array and keep track of the minimum value during each iteration
+// At the end of each iteration, the variables swap
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {2,9,1,8,5,6,3,4,7};
+
+        selectionSort(array);
+
+        for(int i : array){
+            System.out.print(i);
+        }
+
+
+    }
+
+    private static void selectionSort(int[] array) {
+        for(int i = 0; i<array.length-1; i++){
+            int min = i;
+            for(int j = i + 1; j <array.length; j++){
+                if(array[min] > array[j]){ 
+                  // if you want to sort by descending order, change > to <
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+}
