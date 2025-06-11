ecd4b546a03c9055462925dd17f2cfc127d4b363
Noah <106123749+nbran3@users.noreply.github.com>
2025-06-11 11:54:57 -0400

    Create Main.java

---
+++ Java/DSA/AdjacencyList/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Graph graph = new Graph();
+        graph.addNode(new Node('a'));
+        graph.addNode(new Node('b'));
+        graph.addNode(new Node('c'));
+        graph.addNode(new Node('d'));
+        graph.addNode(new Node('e'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(1,4);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,2);
+
+        graph.print();
+
+
+    }
+}
